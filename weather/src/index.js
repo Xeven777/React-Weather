ceda029a2f6bc30ad8f95a8ec1fcb9bf018e4a74
Restyled.io <commits@restyled.io>
2024-01-09 14:36:59 +0000

    Restyled by prettier

--- weather/src/index.js
+++ weather/src/index.js
@@ -1,14 +1,13 @@
-import './index.css';
+import "./index.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
